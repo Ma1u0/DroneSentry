4b57da2ee2504aa1555aaec415cb7b1fcd265f70
Matu <90573043+Ma1u0@users.noreply.github.com>
2026-09-15 15:49:14 +0200

    Update incidents-data.js

--- incidents-data.js
+++ incidents-data.js
@@ -18,7 +18,7 @@ const incidentsData = [
     date: '03 Oct 2025, before 01:45',
     details: "15 drones were spotted by chance due to the test of anti drone equipment was tested",
     link: "https://www.vrt.be/vrtnws/en/2025/10/03/drones-spotted-above-belgian-military-bases/",
-    country: "Elsenborn Military Base, Belgium ðŸ‡§ðŸ‡ª",
+    country: "Elsenborn Military Base, Belgium 🇧🇪",
     note: "The Belgian security service considers Russia to be behind the drones",
     noteLink: "https://www.vrt.be/vrtnws/en/2025/11/05/belgian-security-services-convinced-russia-is-behind-the-drone-i/"
   },
@@ -300,6 +300,12 @@ const incidentsData = [
       details: "The airport was closed following a drone sighting over the airport",
       link: "https://vrtnws.be/p.Pqn1MwxDy#bGl2ZWJsb2dfaXRlbV8xNzYyNDYyMDI5NDQw"
     }
+   {
+      popupType: 'Drone sighting',
+      date: '12 Sep 2026, 19:50',
+      details: "A drone was detected within the critical safety zone of the runways causing flights to be diverted to a different runway. So far it is not suspected that a foreign actor is behind the incident. ",
+      link: "https://www.brusselstimes.com/brussels/2317057/drone-flying-over-brussels-airport-forces-three-flights-to-divert"
+    }
   ]
 },
 
@@ -384,6 +390,21 @@ const incidentsData = [
     country: "Doel Nuclear Power Plant, Belgium 🇧🇪",
     note: "The Belgian security service considers Russia to be behind the drones",
     noteLink: "https://www.vrt.be/vrtnws/en/2025/11/05/belgian-security-services-convinced-russia-is-behind-the-drone-i/"
+  },
+
+   {
+    lat: 50.887778,
+    lng: 4.469167,
+    type: 'drone',      // used for filtering
+    risk: 'yellow',
+    place: 'infrastructure',
+    year: '2026',
+    month: '09',
+    popupType: 'Drone sighting', // shown in popup
+    date: ' 11 Sep 2025, 21:45 ',
+    details: "A drone was spotted in Zaventem causing the airport to be closed as a precautionary measure. Three airplanes were diverted. The drone was seen both visually and by radar systems. So far it is not suspected that a foreign actor is behind the incident. ",
+    link: "https://www.brusselstimes.com/brussels/2317057/drone-flying-over-brussels-airport-forces-three-flights-to-divert",
+    country: "Zaventem, Belgium 🇧🇪",
   },
 
   /* =========================
