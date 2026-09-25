0bc6d75d491e27faa7e11810ca20280aab6a3d92
Matu <90573043+Ma1u0@users.noreply.github.com>
2026-09-25 16:43:39 +0200

    Update incidents-data.js

--- incidents-data.js
+++ incidents-data.js
@@ -919,7 +919,7 @@ const incidentsData = [
     type: 'drone',      // used for filtering
     risk: 'yellow',
     place: 'militarybases',
-    year: ['2025',],
+    year: ['2025'],
     month: ['09', '10'],
     country: "Mourmelon-le-Grand, France 🇫🇷",
 
@@ -934,7 +934,7 @@ const incidentsData = [
      },
     {
       popupType: 'Drone sighting',
-      date: '02 Oct 2026, evening',
+      date: '02 Oct 2025, evening',
       details: "Three unidentified drones were seen over the 10,000 hectare base",
       link: "https://www.lemonde.fr/economie/article/2026/03/09/en-france-face-a-la-montee-de-la-menace-la-lutte-anti-drones-passe-a-l-echelle-industrielle_6670045_3234.html?utm_source=chatgpt.com"
     },
